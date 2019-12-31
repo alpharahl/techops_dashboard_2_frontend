e15264858797f7c1031a6aa04eb88b1cab40c8d2
David Gauntlett <david.m.gauntlett@gmail.com>
2019-12-30 23:05:37 -0500

    begging work on barcode

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+import departments from "@/store/modules/departments";
+
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    departments
+  }
+})
